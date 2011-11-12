bbb218e6408ffa8ced35d6b917cab7b65d92a17e
SND\CodingRoad_cp <SND\CodingRoad_cp@2abf6ff4-378a-4344-b6f7-2bcd8b316ba9>
2011-11-12 17:33:04 +0000

    1. 修改控制項定位
    git-svn-id: https://notehighlight.svn.codeplex.com/svn@8052 2abf6ff4-378a-4344-b6f7-2bcd8b316ba9

--- NoteHighLightAddins/NoteHighLightForm/CodeForm.Designer.cs
+++ NoteHighLightAddins/NoteHighLightForm/CodeForm.Designer.cs
@@ -30,28 +30,26 @@
         {
             this.btnCodeHighLight = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             this.cbx_style = new System.Windows.Forms.ComboBox();
             this.cbx_Clipboard = new System.Windows.Forms.CheckBox();
             this.cbx_lineNumber = new System.Windows.Forms.CheckBox();
-            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
             this.txtCode = new ICSharpCode.TextEditor.TextEditorControl();
             this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
-            this.splitContainer1.Panel1.SuspendLayout();
-            this.splitContainer1.Panel2.SuspendLayout();
-            this.splitContainer1.SuspendLayout();
-            this.splitContainer2.Panel1.SuspendLayout();
-            this.splitContainer2.Panel2.SuspendLayout();
-            this.splitContainer2.SuspendLayout();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnCodeHighLight
             // 
             this.btnCodeHighLight.DialogResult = System.Windows.Forms.DialogResult.OK;
             this.btnCodeHighLight.Dock = System.Windows.Forms.DockStyle.Right;
-            this.btnCodeHighLight.Location = new System.Drawing.Point(509, 0);
+            this.btnCodeHighLight.Location = new System.Drawing.Point(413, 0);
             this.btnCodeHighLight.Name = "btnCodeHighLight";
-            this.btnCodeHighLight.Size = new System.Drawing.Size(75, 40);
+            this.btnCodeHighLight.Size = new System.Drawing.Size(104, 46);
             this.btnCodeHighLight.TabIndex = 0;
             this.btnCodeHighLight.Text = "OK(&O)";
             this.btnCodeHighLight.UseVisualStyleBackColor = true;
@@ -60,32 +58,11 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(19, 12);
+            this.label1.Location = new System.Drawing.Point(28, 19);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(40, 12);
             this.label1.TabIndex = 0;
             this.label1.Text = "Style：";
-            // 
-            // splitContainer1
-            // 
-            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
-            this.splitContainer1.Name = "splitContainer1";
-            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
-            // 
-            // splitContainer1.Panel1
-            // 
-            this.splitContainer1.Panel1.Controls.Add(this.cbx_style);
-            this.splitContainer1.Panel1.Controls.Add(this.label1);
-            this.splitContainer1.Panel1.Controls.Add(this.cbx_Clipboard);
-            this.splitContainer1.Panel1.Controls.Add(this.cbx_lineNumber);
-            // 
-            // splitContainer1.Panel2
-            // 
-            this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
-            this.splitContainer1.Size = new System.Drawing.Size(584, 494);
-            this.splitContainer1.SplitterDistance = 39;
-            this.splitContainer1.TabIndex = 0;
             // 
             // cbx_style
             // 
@@ -100,7 +77,7 @@
             "bright",
             "bclear",
             "edit-msvs2008"});
-            this.cbx_style.Location = new System.Drawing.Point(68, 7);
+            this.cbx_style.Location = new System.Drawing.Point(74, 16);
             this.cbx_style.Name = "cbx_style";
             this.cbx_style.Size = new System.Drawing.Size(121, 20);
             this.cbx_style.TabIndex = 0;
@@ -110,10 +87,10 @@
             this.cbx_Clipboard.AutoSize = true;
             this.cbx_Clipboard.Checked = global::NoteHighLightForm.Properties.Settings.Default.SaveOnClipboard;
             this.cbx_Clipboard.DataBindings.Add(new System.Windows.Forms.Binding("Checked", global::NoteHighLightForm.Properties.Settings.Default, "SaveOnClipboard", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
-            this.cbx_Clipboard.Location = new System.Drawing.Point(448, 12);
+            this.cbx_Clipboard.Location = new System.Drawing.Point(271, 18);
             this.cbx_Clipboard.Name = "cbx_Clipboard";
             this.cbx_Clipboard.Size = new System.Drawing.Size(128, 16);
-            this.cbx_Clipboard.TabIndex = 2;
+            this.cbx_Clipboard.TabIndex = 1;
             this.cbx_Clipboard.Text = "Copy to Clipboard(&C)";
             this.cbx_Clipboard.UseVisualStyleBackColor = true;
             // 
@@ -123,30 +100,12 @@
             this.cbx_lineNumber.Checked = global::NoteHighLightForm.Properties.Settings.Default.ShowLineNumber;
             this.cbx_lineNumber.CheckState = System.Windows.Forms.CheckState.Checked;
             this.cbx_lineNumber.DataBindings.Add(new System.Windows.Forms.Binding("Checked", global::NoteHighLightForm.Properties.Settings.Default, "ShowLineNumber", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
-            this.cbx_lineNumber.Location = new System.Drawing.Point(338, 12);
+            this.cbx_lineNumber.Location = new System.Drawing.Point(405, 18);
             this.cbx_lineNumber.Name = "cbx_lineNumber";
             this.cbx_lineNumber.Size = new System.Drawing.Size(102, 16);
-            this.cbx_lineNumber.TabIndex = 1;
+            this.cbx_lineNumber.TabIndex = 2;
             this.cbx_lineNumber.Text = "Line Number(&N)";
             this.cbx_lineNumber.UseVisualStyleBackColor = true;
-            // 
-            // splitContainer2
-            // 
-            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
-            this.splitContainer2.Name = "splitContainer2";
-            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
-            // 
-            // splitContainer2.Panel1
-            // 
-            this.splitContainer2.Panel1.Controls.Add(this.txtCode);
-            // 
-            // splitContainer2.Panel2
-            // 
-            this.splitContainer2.Panel2.Controls.Add(this.btnCodeHighLight);
-            this.splitContainer2.Size = new System.Drawing.Size(584, 451);
-            this.splitContainer2.SplitterDistance = 407;
-            this.splitContainer2.TabIndex = 0;
             // 
             // txtCode
             // 
@@ -155,27 +114,56 @@
             this.txtCode.IsReadOnly = false;
             this.txtCode.Location = new System.Drawing.Point(0, 0);
             this.txtCode.Name = "txtCode";
-            this.txtCode.Size = new System.Drawing.Size(584, 407);
+            this.txtCode.Size = new System.Drawing.Size(517, 351);
             this.txtCode.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.cbx_lineNumber);
+            this.panel1.Controls.Add(this.cbx_Clipboard);
+            this.panel1.Controls.Add(this.cbx_style);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(517, 49);
+            this.panel1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.btnCodeHighLight);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 400);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(517, 46);
+            this.panel2.TabIndex = 2;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.txtCode);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(0, 49);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(517, 351);
+            this.panel3.TabIndex = 0;
             // 
             // CodeForm
             // 
             this.AcceptButton = this.btnCodeHighLight;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(584, 494);
-            this.Controls.Add(this.splitContainer1);
+            this.ClientSize = new System.Drawing.Size(517, 446);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.Name = "CodeForm";
             this.Text = "NoteHighLight";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.CodeForm_FormClosed);
             this.Load += new System.EventHandler(this.CodeForm_Load);
-            this.splitContainer1.Panel1.ResumeLayout(false);
-            this.splitContainer1.Panel1.PerformLayout();
-            this.splitContainer1.Panel2.ResumeLayout(false);
-            this.splitContainer1.ResumeLayout(false);
-            this.splitContainer2.Panel1.ResumeLayout(false);
-            this.splitContainer2.Panel2.ResumeLayout(false);
-            this.splitContainer2.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -186,11 +174,12 @@
         private System.Windows.Forms.CheckBox cbx_lineNumber;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.CheckBox cbx_Clipboard;
-        private System.Windows.Forms.SplitContainer splitContainer1;
-        private System.Windows.Forms.SplitContainer splitContainer2;
         private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
         private System.Windows.Forms.ComboBox cbx_style;
         private ICSharpCode.TextEditor.TextEditorControl txtCode;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
     }
 }
 
